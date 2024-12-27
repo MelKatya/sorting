4b2c3ac220576fc9b75fdc8f41289530c9cd6f26
Katerina <mel.katya@mail.ru>
2024-12-27 23:48:43 +0300

    timer

---
+++ timer_dec.py
@@ -0,0 +1,17 @@
+from functools import wraps
+import time
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        total_time = round(end_time - start_time, 6)
+        print('Время выполнения функции "{func}" составляет {time} секунд'.format(
+            func=func.__name__,
+            time=total_time
+        ))
+        return result
+    return wrapper
